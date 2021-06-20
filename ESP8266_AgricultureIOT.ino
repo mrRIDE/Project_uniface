6c28a52f3f1364b046685d6eb5bef796b0b102a7
mrRIDE <thanthanh1203@gmail.com>
2021-06-20 09:15:43 +0900

    Rev3. Smple Control Syssem create

--- ESP8266_AgricultureIOT.ino
+++ ESP8266_AgricultureIOT.ino
@@ -35,17 +35,33 @@
 
 /*-------------------------------------------------------------*/
 /* Define IO Port */
-#define OLED_SCL	9			//GPIO 9
-#define OLED_SDA	10			//GPIO 10
+#define OLED_SCL	9			//GPIO9
+#define OLED_SDA	10			//GPIO10
 #define OLED_ID		0x3C		//I2c Address
 
-#define DHT_OUT		12			//GPIO 12
+#define DHT_PIN		12			//GPIO12
 #define DHT_TYPE	DHT11
+
+#define CTR_PUMP	5			//GPIO5
+#define CTR_LIGHT	0			//GPIO0
+#define CTR_BUTTON	4			//GPIO4
+
+#define CTR_MODE_AUTO		0
+#define CTR_MODE_MANU_PUMP	1
+#define CTR_MODE_MANU_LIGH	2
+
+#define CTR_TEMP_MIN	17
+#define CTR_TEMP_MAX	20
+#define CTR_HUMD_MIN	70
+#define CTR_HUMD_MAX	80
+
+#define ON				1
+#define OFF				0
 
 /*-------------------------------------------------------------*/
 /* Config Device */
 SSD1306 displayOLED(OLED_ID, OLED_SDA, OLED_SCL);
-DHT sensorDHT(DHT_OUT, DHT_TYPE);
+DHT sensorDHT(DHT_PIN, DHT_TYPE);
 
 Ticker ticker10ms;
 Ticker ticker100ms;
@@ -56,6 +72,12 @@ struct sensor_dht_data
 	float temp;
 } DHTDATA;
 
+struct control_mode
+{
+	uint8_t ctrMode;			//0: Auto, 1: Manual
+	uint8_t pumpSts;
+	uint8_t lightSts;
+} CTRLSYSTEM;
 /*-------------------------------------------------------------*/
 /* Function Prototype */
 void systemInit();
@@ -64,15 +86,21 @@ void task10ms_Schedule();
 void task100ms_Schedule();
 
 void readSensorDHT();
-void displayTempHumd();
+void displaySystemInfor();
 void blinkLED();
+bool isButtonPress_3000ms();
+bool isButtonPress_150ms();
+void controlAutoMode();
+void controlManualMode(uint8_t manu_mode);
+void checkChangeMode();
+void selectControlMode();
 
 /*-------------------------------------------------------------*/
 /* Task List */
 void task10ms_Schedule()
 {
-	//wait for read 
 	//Serial.printf("task10ms: %d \n", millis());
+	mainControl();
 
 }
 
@@ -81,7 +109,7 @@ void task100ms_Schedule()
 	//Serial.printf("task100ms: %d \n", millis());
 	blinkLED();
 	readSensorDHT();
-	displayTempHumd();
+	displaySystemInfor();
 
 }
 
@@ -97,66 +125,197 @@ void setup() {
 // the loop function runs over and over again until power down or reset
 void loop() {
 	//Use Ticker 10ms. 100ms for control OS
-	
+
 }
 
 /*-------------------------------------------------------------*/
-void variableInit()
+void mainControl()
 {
-	DHTDATA.humd = 0;
-	DHTDATA.temp = 0;
+	checkChangeMode();
+	selectControlMode();
 
 }
 
-void displayTempHumd()
+void selectControlMode()
+{
+	switch (CTRLSYSTEM.ctrMode)
+	{
+	case CTR_MODE_AUTO:
+		controlAutoMode();
+		break;
+	case CTR_MODE_MANU_PUMP:
+		controlManualMode(CTR_MODE_MANU_PUMP);
+		break;
+	case CTR_MODE_MANU_LIGH:
+		controlManualMode(CTR_MODE_MANU_LIGH);
+		break;
+	default:
+		break;
+	}
+}
+
+void checkChangeMode()
+{
+	if (isButtonPress_3000ms())
+	{
+		switch (CTRLSYSTEM.ctrMode)
+		{
+		case CTR_MODE_AUTO:
+			CTRLSYSTEM.ctrMode = CTR_MODE_MANU_PUMP;
+			break;
+		case CTR_MODE_MANU_PUMP:
+			CTRLSYSTEM.ctrMode = CTR_MODE_MANU_LIGH;
+			break;
+		case CTR_MODE_MANU_LIGH:
+			CTRLSYSTEM.ctrMode = CTR_MODE_AUTO;	//return auto
+			break;
+		default:
+			break;
+		}
+	}
+
+}
+
+void controlAutoMode()
+{
+	if (DHTDATA.temp < CTR_TEMP_MIN)
+	{
+		digitalWrite(CTR_LIGHT, LOW);		//turn on light
+		CTRLSYSTEM.lightSts = digitalRead(CTR_LIGHT);
+	}
+	else if (DHTDATA.temp > CTR_LIGHT)
+	{
+		digitalWrite(CTR_PUMP, HIGH);		//turn off light
+		CTRLSYSTEM.lightSts = digitalRead(CTR_LIGHT);
+	}
+
+	if (DHTDATA.humd < CTR_HUMD_MIN)
+	{
+		digitalWrite(CTR_PUMP, LOW);		//turn on pump
+		CTRLSYSTEM.pumpSts = digitalRead(CTR_PUMP);
+	}
+	else if (DHTDATA.humd > CTR_PUMP)
+	{
+		digitalWrite(CTR_PUMP, HIGH);		//turn off pump
+		CTRLSYSTEM.pumpSts = digitalRead(CTR_PUMP);
+	}
+
+}
+
+void controlManualMode(uint8_t manu_mode)
+{
+	if (isButtonPress_150ms())
+	{
+		switch (manu_mode)
+		{
+		case CTR_MODE_MANU_PUMP:
+			digitalWrite(CTR_PUMP, !digitalRead(CTR_PUMP));
+			CTRLSYSTEM.pumpSts = digitalRead(CTR_PUMP);
+			break;
+		case CTR_MODE_MANU_LIGH:
+			digitalWrite(CTR_LIGHT, !digitalRead(CTR_LIGHT));
+			CTRLSYSTEM.lightSts = digitalRead(CTR_LIGHT);
+			break;
+		default:
+			break;
+		}
+	}
+}
+
+bool isButtonPress_150ms()
+{
+	static uint16_t pressCnt = 0;
+
+	//Serial.printf("press100ms: %d, mili: %d \n", pressCnt, millis());
+	if (digitalRead(CTR_BUTTON) == 0)
+	{
+		pressCnt++;
+		//press time 15*os10ms=150ms
+		if (pressCnt > 15)
+		{
+			pressCnt = 0;
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	else
+	{
+		pressCnt = 0;
+		return false;
+	}
+
+}
+
+bool isButtonPress_3000ms()
+{
+	static uint16_t pressCnt = 0;
+
+	//Serial.printf("press3000ms: %d, mili: %d \n", pressCnt, millis());
+	if (digitalRead(CTR_BUTTON) == 0)
+	{
+		pressCnt++;
+		//press time 300*os10ms=3000ms
+		if (pressCnt > 300)
+		{
+			pressCnt = 0;
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	else
+	{
+		pressCnt = 0;
+		return false;
+	}
+
+}
+
+void displaySystemInfor()
 {
 	displayOLED.clear();
-	displayOLED.drawString(0, 5, "Farm System");
-	displayOLED.drawString(0, 20, "Temp: " + String(DHTDATA.temp, 1) + " *C");
-	displayOLED.drawString(0, 35, "Humd: " + String(DHTDATA.humd, 1) + " %");
+	displayOLED.setFont(ArialMT_Plain_10);
+	displayOLED.drawString(0, 0, "__FARM SYSTEM__");
+	displayOLED.drawString(0, 10, "Mode: " + returnStrCtrMode(CTRLSYSTEM.ctrMode));
+	displayOLED.drawString(0, 20, "Temp:" + String(DHTDATA.temp, 1) + "*C");
+	displayOLED.drawString(0, 30, "Humd:" + String(DHTDATA.humd, 1) + "%");
+	displayOLED.drawString(0, 40, "PumpSts: " + String(!CTRLSYSTEM.pumpSts) + "LighSts: " + String(!CTRLSYSTEM.lightSts));
 	displayOLED.display();
-	
+
 }
 
-void systemInit()
+String returnStrCtrMode(uint8_t control_mode)
 {
-	// Init Serial for debug
-	Serial.begin(115200);
-	Serial.println("Init Serial");
+	String rteString;
 
-	// Init Port IO
-	pinMode(LED_BUILTIN, OUTPUT);
-	delay(50);
-	digitalWrite(LED_BUILTIN, HIGH);		//Set OFF
-	Serial.println("Init IO port");
+	switch (control_mode)
+	{
+	case CTR_MODE_AUTO:
+		rteString = "Auto";
+		break;
+	case CTR_MODE_MANU_LIGH:
+		rteString = "Manu-Light";
+		break;
+	case CTR_MODE_MANU_PUMP:
+		rteString = "Manu-Pump";
+		break;
+	default:
+		break;
+	}
 
-	// Init OLED
-	displayOLED.init();
-	delay(50);
-	displayOLED.flipScreenVertically();
-	displayOLED.setFont(ArialMT_Plain_16);
-	displayOLED.drawString(0, 5, "_WELCOME_");
-	displayOLED.drawString(0, 20, "_Farm_");
-	displayOLED.drawString(0, 35, "_System_");
-	displayOLED.display();
-	Serial.println("Init OLED");
-
-	// Init Sensor DHT
-	sensorDHT.begin();
-	delay(50);
-	readSensorDHT();
-	Serial.println("Init Sensor DHT");
-	Serial.printf("Frist read: Temp: %.1f(*C) - Humd: %.1f(%%)", DHTDATA.temp, DHTDATA.humd);
-
-	delay(2000);
-
+	return rteString;
 }
 
 void readSensorDHT()
 {
 	float temp_t;
 	float humd_t;
-	
+
 	temp_t = sensorDHT.readTemperature();
 	humd_t = sensorDHT.readHumidity();
 
@@ -182,4 +341,51 @@ void readSensorDHT()
 void blinkLED()
 {
 	digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+}
+
+void variableInit()
+{
+	DHTDATA.humd = 0;
+	DHTDATA.temp = 0;
+
+	CTRLSYSTEM.ctrMode = CTR_MODE_AUTO;
+}
+
+void systemInit()
+{
+	// Init Serial for debug
+	Serial.begin(115200);
+	Serial.println("Init Serial");
+
+	// Init Port IO
+	pinMode(LED_BUILTIN, OUTPUT);
+	pinMode(CTR_PUMP, OUTPUT);
+	pinMode(CTR_LIGHT, OUTPUT);
+	pinMode(CTR_BUTTON, INPUT);
+
+	digitalWrite(LED_BUILTIN, HIGH);		//Set OFF
+	digitalWrite(CTR_LIGHT, HIGH);
+	digitalWrite(CTR_PUMP, HIGH);
+	Serial.println("Init IO port");
+
+	// Init OLED
+	displayOLED.init();
+	delay(50);
+	displayOLED.flipScreenVertically();
+	displayOLED.setFont(ArialMT_Plain_16);
+	displayOLED.drawString(0, 5, "_WELCOME_");
+	displayOLED.drawString(0, 20, "_Farm_");
+	displayOLED.drawString(0, 35, "_System_");
+	displayOLED.display();
+	Serial.println("Init OLED");
+
+	// Init Sensor DHT
+	sensorDHT.begin();
+	delay(50);
+	readSensorDHT();
+	Serial.println("Init Sensor DHT");
+	Serial.printf("Frist read: Temp: %.1f(*C) - Humd: %.1f(%%)", DHTDATA.temp, DHTDATA.humd);
+
+	delay(2000);
+
 }
